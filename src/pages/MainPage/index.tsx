771826ae1c6f8af546a748a3db8fb8a98b472247
Dima <linklib.web@yandex.ru>
2022-05-30 08:51:58 +0300

    Test page

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,12 +2,37 @@ import { NextSeo } from 'next-seo'
 
 import { Page } from '../_App/interfaces'
 
+import { useState } from 'react'
+
+
+
 export const MainPage: Page = () => {
+
+const TestPage = () => {
+  const [name, setName] = useState('')
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Your Name"
+        />
+        <p>{name}</p>
+      </form>
+    </div>
+  )
+}
+
+
+
   return (
     <>
       <NextSeo title="Main page" description="Main page description" />
-
       <div>Main Page 123</div>
+      <TestPage/>
     </>
   )
 }
