25ee9acf884b52bcdea6a6fe225eff4aba55f683
Dima <linklib.web@yandex.ru>
2022-05-30 09:49:57 +0300

    Test page 3

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,37 +2,39 @@ import { NextSeo } from 'next-seo'
 
 import { Page } from '../_App/interfaces'
 
-import { useState } from 'react'
-
-
+import { useState, useCallback } from 'react'
 
 export const MainPage: Page = () => {
+  const TestPage = () => {
+    const [name, setName] = useState('')
 
-const TestPage = () => {
-  const [name, setName] = useState('')
+    const onChange = useCallback(
+      (e) => {
+        setName(e.target.value)
+      },
+      [setName]
+    )
 
-  return (
-    <div>
-      <form>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Your Name"
-        />
-        <p>{name}</p>
-      </form>
-    </div>
-  )
-}
-
-
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            value={name}
+            onChange={onChange}
+            placeholder="Your Name 1"
+          />
+          <p>{name}</p>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <>
       <NextSeo title="Main page" description="Main page description" />
       <div>Main Page 123</div>
-      <TestPage/>
+      <TestPage />
     </>
   )
 }
